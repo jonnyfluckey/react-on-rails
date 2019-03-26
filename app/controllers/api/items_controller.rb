82cd0566222d7177d5b3db1f23890716a278486a
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-03-25 19:12:59 -0600

    added controllers and model

---
+++ app/controllers/api/items_controller.rb
@@ -0,0 +1,61 @@
+class Api::ItemsController < ApplicationController
+
+  # before_action :set_item, only: [:show, :destroy, :update, :edit]
+
+  # READ
+  def index
+    render json: Item.all
+
+  end
+
+  # def show
+
+  # end
+
+  # def new
+  #   @item = Item.new
+  # end
+
+  # def edit
+
+  # end
+
+  # CREATE
+  def create
+    @item=Item.new(item_params)
+
+    if @item.save
+      render json: @item
+    else
+      render json: { errors: item.errors }, status :unprocessable_entity
+    end
+  end
+# UPDATE
+  def update
+    # if @item.update(item_params)
+    #   render json: @item
+    # else
+    #   render json: { errors: @item.errors }. status :unprocessable_entity
+    # end
+    item = Item.find(params[:id])
+    item.update(complete: !item.complete)
+    render json: item
+  end
+
+  # DELETE
+  def destroy
+    Item.find(params[:id]).destroy
+    render json: {message: 'item is deleted'}
+  end
+
+  private
+
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
+
+  def item_params
+    params.require(:item).permit(:name, :complete)
+  end
+  
+end
